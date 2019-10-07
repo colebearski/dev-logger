da42feb8cffbd88123b8586e68ca5b479029f307
Cole Pedersen <colep@lendio.com>
2019-10-06 19:33:35 -0600

    ported hard data over to comp thru service

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,26 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LogService } from '../../services/log.service';
+
+import { Log } from '../../models/Log';
+
 @Component({
   selector: 'app-logs',
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
-  logs: {
-    id: string,
-    text: string,
-    date: any
-  }[];
+  logs: Log[];
 
-  constructor() { }
+  constructor(private logService: LogService) { }
 
   ngOnInit() {
-    this.logs = [
-      {id: '1', text: 'Gen components', date: new Date('12/26/2019 12:54:22')},
-      {id: '2', text: 'Dat new Ng', date: new Date('10/6/2019 12:58:22')},
-      {id: '3', text: 'Chilling with Java', date: new Date('2/26/2019 12:50:22')}
-      // props ^^
-    ]
+    this.logs = this.logService.getLogs();
   }
 
 }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import { Log } from '../models/Log';
+
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
+  logs: Log[];
 
-  constructor() { }
+  constructor() { 
+    this.logs = [
+      {id: '1', text: 'Gen components', date: new Date('12/26/2019 12:54:22')},
+      {id: '2', text: 'Dat new Ng', date: new Date('10/6/2019 12:58:22')},
+      {id: '3', text: 'Chilling with Java', date: new Date('2/26/2019 12:50:22')}
+    ]
+  }
+
+  getLogs() {
+    return this.logs;
+  }
 }
