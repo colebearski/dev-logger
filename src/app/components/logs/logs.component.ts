dfe09afc9f1a9cc4214857c4a645b52e25cd6180
Cole Pedersen <colep@lendio.com>
2019-10-06 19:19:52 -0600

    hard coded log properties

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -19,6 +19,7 @@ export class LogsComponent implements OnInit {
       {id: '1', text: 'Gen components', date: new Date('12/26/2019 12:54:22')},
       {id: '2', text: 'Dat new Ng', date: new Date('10/6/2019 12:58:22')},
       {id: '3', text: 'Chilling with Java', date: new Date('2/26/2019 12:50:22')}
+      // props ^^
     ]
   }
 
