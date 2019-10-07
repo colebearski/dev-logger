1ad6bf8590c0fe32955226dcbd954e11d931ac6a
Cole Pedersen <colep@lendio.com>
2019-10-06 19:19:23 -0600

    hard coded logs data

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
+  logs: {
+    id: string,
+    text: string,
+    date: any
+  }[];
 
   constructor() { }
 
   ngOnInit() {
+    this.logs = [
+      {id: '1', text: 'Gen components', date: new Date('12/26/2019 12:54:22')},
+      {id: '2', text: 'Dat new Ng', date: new Date('10/6/2019 12:58:22')},
+      {id: '3', text: 'Chilling with Java', date: new Date('2/26/2019 12:50:22')}
+    ]
   }
 
 }
